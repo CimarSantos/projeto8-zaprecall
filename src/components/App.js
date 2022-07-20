df7d7edb67239411edc6308fc89bcf077b7e05d5
CimarSantos <francimarlsantos@outlook.com>
2022-07-19 21:39:59 -0300

    Projeto Base

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import "../assets/styles/reset.css";
+import "../assets/styles/style.css";
+
+function App() {
+  return (
+    <>
+      <h1>Hello World!</h1>
+    </>
+  );
+}
+
+export default App;
